a4513481dc4e36fec7d679bb260977988895811b
C <chronocrusaderagain@yahoo.com>
2025-03-20 08:41:20 -0400

    Initial Commit

---
+++ watchers Watch App/watchersApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchersApp.swift
+//  watchers Watch App
+//
+//  Created by Cameron Slaughter on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct watchers_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchers Watch AppTests/watchers_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  watchers_Watch_AppTests.swift
+//  watchers Watch AppTests
+//
+//  Created by Cameron Slaughter on 3/20/25.
+//
+
+import Testing
+@testable import watchers_Watch_App
+
+struct watchers_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
