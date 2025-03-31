e75b0084aa54284a4811f6f893b6c30c34c813f4
C <chronocrusaderagain@yahoo.com>
2025-03-31 10:23:42 -0400

    Added home page and some calendar stuff

---
+++ watchers Watch App/SetSchedule.swift
@@ -0,0 +1,18 @@
+//
+//  SetSchedule.swift
+//  watchers Watch App
+//
+//  Created by Cameron Slaughter on 3/31/25.
+//
+
+import SwiftUI
+
+struct SetSchedule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetSchedule()
+}
